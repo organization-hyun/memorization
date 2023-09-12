1036fc3c6a08156ef9f87778a2a7aa770ced652c
hyunjuuun <hyunjun86120@gmail.com>
2023-09-12 19:35:34 +0900

    Initial commit

---
+++ src/main/java/com/hyunjuuun/memorization/MemorizationApplication.java
@@ -0,0 +1,13 @@
+package com.hyunjuuun.memorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunjuuun/memorization/MemorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunjuuun.memorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
