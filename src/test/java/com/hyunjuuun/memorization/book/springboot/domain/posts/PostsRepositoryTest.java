aa507788c4729221ed3b21aba8448d3f39a7b8f4
hyunjuuun <hyunjun86120@gmail.com>
2023-09-16 23:58:01 +0900

    @EnableJpaAuditing 설정, BaseTimeEntity 추가

--- src/test/java/com/hyunjuuun/memorization/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/hyunjuuun/memorization/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,6 +42,27 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+    @DisplayName("BaseTimeEntity 등록")
+    @Test
+    public void testBaseTimeEntity() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023, 9, 16, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
 
     }
 
