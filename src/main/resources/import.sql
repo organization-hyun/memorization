a62f5ca37dbc8a052fd8fd069fa8ebbd6cc21485
Hg-lab <siregy1222@gmail.com>
2023-09-26 00:23:49 +0900

    docs: init local test sample data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+insert into GLOSSARY (title) values ( '타이틀1' );
+insert into GLOSSARY (title) values ( '타이틀2' );
+insert into GLOSSARY (title) values ( '타이틀3' );
+
+insert into Term (glossary_id, word, description) values ( 1, '용어1', '설명1~~~~~~~~~' );
+insert into Term (glossary_id, word, description) values ( 2, '용어2', '설명2~~~~~~~~~' );
+insert into Term (glossary_id, word, description) values ( 2, '용어3', '설명3~~~~~~~~~' );
+insert into Term (glossary_id, word, description) values ( 3, '용어4', '설명4~~~~~~~~~' );
+insert into Term (glossary_id, word, description) values ( 3, '용어5', '설명5~~~~~~~~~' );
+insert into Term (glossary_id, word, description) values ( 3, '용어6', '설명6~~~~~~~~~' );
