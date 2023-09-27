6b03998778f412fce931b03d74195b1be0792ea3
Hg-lab <siregy1222@gmail.com>
2023-09-27 17:49:26 +0900

    chore: init test data file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,16 @@ insert into Term (glossary_id, word, description) values ( 2, '용어3', '설명
 insert into Term (glossary_id, word, description) values ( 3, '용어4', '설명4~~~~~~~~~' );
 insert into Term (glossary_id, word, description) values ( 3, '용어5', '설명5~~~~~~~~~' );
 insert into Term (glossary_id, word, description) values ( 3, '용어6', '설명6~~~~~~~~~' );
+
+insert into keywords (term_id, keyword) values ( 1, '키워드1');
+insert into keywords (term_id, keyword) values ( 1, '키워드2');
+insert into keywords (term_id, keyword) values ( 2, '키워드3');
+insert into keywords (term_id, keyword) values ( 3, '키워드4');
+insert into keywords (term_id, keyword) values ( 3, '키워드5');
+insert into keywords (term_id, keyword) values ( 3, '키워드6');
+insert into keywords (term_id, keyword) values ( 4, '키워드7');
+insert into keywords (term_id, keyword) values ( 4, '키워드8');
+insert into keywords (term_id, keyword) values ( 5, '키워드9');
+insert into keywords (term_id, keyword) values ( 5, '키워드10');
+insert into keywords (term_id, keyword) values ( 6, '키워드11');
+insert into keywords (term_id, keyword) values ( 6, '키워드12');
