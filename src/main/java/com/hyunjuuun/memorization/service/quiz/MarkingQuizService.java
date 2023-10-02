f80a44b4f3916cd195946a700be85317a5ae8ec1
Hg-lab <siregy1222@gmail.com>
2023-10-03 00:46:09 +0900

    feat: quiz 서비스 레이어 스켈레톤 코드

---
+++ src/main/java/com/hyunjuuun/memorization/service/quiz/MarkingQuizService.java
@@ -0,0 +1,11 @@
+package com.hyunjuuun.memorization.service.quiz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Quiz를 채점한다
+ */
+
+@Service
+public class MarkingQuizService {
+}
---
+++ src/main/java/com/hyunjuuun/memorization/service/quiz/QuizService.java
@@ -0,0 +1,11 @@
+package com.hyunjuuun.memorization.service.quiz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * glossaryId를 이용해 문제를 출제한다
+ */
+
+@Service
+public class QuizService {
+}
