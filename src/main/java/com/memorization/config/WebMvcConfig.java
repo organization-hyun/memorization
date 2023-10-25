296b4e71b53e45d644186e5fe1905aeea0469a21
Hg-lab <siregy1222@gmail.com>
2023-10-26 00:54:35 +0900

    build: 운영 환경 분리 CORS_URL, ddl-auto 변경

--- src/main/java/com/memorization/config/WebMvcConfig.java
+++ src/main/java/com/memorization/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.memorization.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +10,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
+    private final String CORS_URL;
+
+    public WebMvcConfig(@Value("${cors.url}") String corsUrl) {
+        CORS_URL = corsUrl;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://web-memorization-front-12fhqa2llo2ulv8z.sel5.cloudtype.app")
+                .allowedOrigins(CORS_URL)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
